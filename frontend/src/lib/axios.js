0332bf39ac4f7d47adbc7ca7f78143f5e6a95926
shivamy009 <shivam0342005@gmail.com>
2025-01-29 10:12:43 +0530

    url change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://quizstack.onrender.com/api",
   withCredentials: true,
 });
