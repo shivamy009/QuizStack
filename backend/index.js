c9d378a71d68ba57d35026f8eab886bc91dda263
shivamy009 <shivam0342005@gmail.com>
2025-01-30 13:42:20 +0530

    url

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://quiz-stack-delta.vercel.app",
     credentials: true,
   })
 );
